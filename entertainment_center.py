c72a4848c3cc24796529b9122454a6a689cd34f8
ababen <alexbaben@gmail.com>
2017-01-06 15:15:13 -0400

    All finished with fundamentals.

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,4 +5,18 @@ toy_story = media.Movie("Toy Story",
                         "toy_story_poster.jpg",
                         "link")
 
+toy_story = media.Movie("Avatar",
+                        "A story of a boy and his toys that come to life",
+                        "toy_story_poster.jpg",
+                        "link")
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "toy_story_poster.jpg",
+                        "link")
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "toy_story_poster.jpg",
+                        "link")
 print(toy_story.storyline)
